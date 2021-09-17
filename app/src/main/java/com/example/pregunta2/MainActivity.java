b83ddd1035e62e284a57ca54b4a80f421e7d0012
jualta <tarazonachacon@gmail.com>
2021-09-17 12:39:56 -0500

    suma,resta,multiplicacion,division

---
+++ app/src/main/java/com/example/pregunta2/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.pregunta2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText val1,val2;
+    TextView res;
+    Button Suma,Resta,Mult,Divi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        val1=(EditText)findViewById(R.id.valor1);
+        val2=(EditText)findViewById(R.id.valor2);
+        res=(TextView) findViewById(R.id.result);
+        Suma=(Button) findViewById(R.id.suma);
+        Resta=(Button) findViewById(R.id.resta);
+        Mult=(Button) findViewById(R.id.multiplicacion);
+        Divi=(Button) findViewById(R.id.division);
+
+       Suma.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               suma();
+           }
+       });
+       Resta.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               resta();
+           }
+       });
+       Mult.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               multiplicacion();
+           }
+       });
+       Divi.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               division();
+           }
+       });
+
+    }
+    private void division(){
+        double x,y;
+        double r;
+        x=Integer.parseInt(val1.getText().toString());
+        y=Integer.parseInt(val2.getText().toString());
+        r=x/y;
+        res.setText(String.valueOf(r));
+    }
+    private void multiplicacion(){
+        double x,y;
+        double r;
+        x=Integer.parseInt(val1.getText().toString());
+        y=Integer.parseInt(val2.getText().toString());
+        r=x*y;
+        res.setText(String.valueOf(r));
+    }
+    private void resta(){
+        double x,y;
+        double r;
+        x=Integer.parseInt(val1.getText().toString());
+        y=Integer.parseInt(val2.getText().toString());
+        r=x-y;
+        res.setText(String.valueOf(r));
+    }
+    private void suma(){
+        double x,y;
+        double r;
+        x=Integer.parseInt(val1.getText().toString());
+        y=Integer.parseInt(val2.getText().toString());
+        r=x+y;
+        res.setText(String.valueOf(r));
+    }
+}
